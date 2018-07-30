80e09dc1d4321b409e6c9b7d85b43bfa0ed9f274
warrenyuan <warreny7853@gmail.com>
2018-07-30 16:55:44 -0700

    test

--- GUI/leave.py
+++ GUI/leave.py
@@ -6,18 +6,18 @@ baud = 9600
 
 serial_object = serial.Serial(port, baud)
 
-def disconnect(usrexit):
-    if serial_object.isOpen():
-        print(serial_object.name + ' is open...')
+
+if serial_object.isOpen():
+    print(serial_object.name + ' is open...')
     
-    while True:
-        cmd = raw_input()
-        if cmd == "e":    
-            print("exiting program and disconnecting from serial")
-            serial_object.close() 
-            sys.exit()
-        else:
-            serial_object(cmd.encode('ascii')+'\r\n')
-            out = serial_object()
-            print('Recieving...' + out)
+while True:
+    cmd = raw_input()
+    if cmd == "e":    
+        print("exiting program and disconnecting from serial")
+        serial_object.close() 
+        sys.exit()
+    else:
+        serial_object(cmd.encode('ascii')+'\r\n')
+        out = serial_object()
+        print('Recieving...' + out)
 
