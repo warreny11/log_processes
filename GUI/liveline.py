ef98f79f6898a413934a79b435208d0ea6c2f930
warrenyuan <warreny7853@gmail.com>
2018-07-30 15:16:34 -0700

    uncombined

--- GUI/liveline.py
+++ GUI/liveline.py
@@ -1,10 +1,8 @@
 import serial
 import time
-import sys 
 
 #port = raw_input("Enter Port Name: ")
 #baud = raw_input("Enter Baud Rate: ")
-usrexit = raw_input()
 
 def connect(): 
     global serial_object
@@ -24,17 +22,6 @@ def connect():
             else:
                 print 'no data'
             time.sleep(1) 
-            import serial
-
-def disconnect(usrexit):
-    if usrexit == "e": 
-        try :   
-            print("exiting program and disconnecting from serial")
-            serial_object.close() 
-            sys.exit()
-        except :
-            print("still connected")
-
 
 print connect()
 
