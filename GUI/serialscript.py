0a99ddee16faaac68049b97f476c19cb08801abc
warrenyuan <warreny7853@gmail.com>
2018-07-31 14:49:55 -0700

    test

--- GUI/serialscript.py
+++ GUI/serialscript.py
@@ -1,62 +1,28 @@
-import liveline
-import data_
-import data_import
+from ser_interface import connect, commands
 import re
 import time
 import data_sort
 
+port = raw_input("Enter Port Name: ")
+baud = raw_input("Enter Baud Rate: ")
 
-def connect(): 
-    global serial_object
-    port = raw_input("Enter Port Name: ")
-    baud = raw_input("Enter Baud Rate: ") 
-
-    serial_object = serial.Serial(str(port), baud)
-
-if serial_object.is_open:
-    while True:
+connect(port,baud)
+if connect(port,baud)==0:
+    print "Connected..."                                   #if connected
+    
+    commands()
+    if commands()==2:
         size = serial_object.inWaiting()
         if size:
             data = serial_object.read(size)
             print data
         else:
-            print 'no data'
-        time.sleep(1)
+            print 'no data'                                       #no data, prints no data
+            time.sleep(1)
 
-def data_sort(liveline):
-    
-    pattern = re.compile(r'\:([^;]*)\;')
-    split_line = pattern.findall(liveline)
-    data = []
-    key = []
-
-    for i in range(len(split_line)):
-        
-        brokendata = split_line[i].split('=')
-        data.append(brokendata[1])
-        key.append(brokendata[0])
-        
-        if key[i] == "VB" :
-            print("Voltage :" + str(data[i]))
-
-        if key[i] == "PR" :
-            print("Presure :" + str(data[i]))
-        
+elif connect(port,baud)==-1:
+    print "Connection not established..."
 
 
-def hex2ascii(hexnum):
-    
-    num = hexnum.decode("hex")
-    return num
-
-
-def leave(userinput):
-    asdf = 1
-
-while(1): 
-    """forever loop until exit function is called"""
-    liveline = serial.readline
-    data_sort(liveline)
-    leave(userinput)
     
 
