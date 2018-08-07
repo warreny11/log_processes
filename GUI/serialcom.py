4ca8c1de947c19526f6447dc02302407e117387a
Warren Yuan <warrenyuan@Warrens-MacBook-Pro.local>
2018-08-07 15:59:51 -0700

    removed much of code

--- GUI/serialcom.py
+++ GUI/serialcom.py
@@ -21,25 +21,9 @@ def connect(a,b):
     else:
         return -1                                                      #0 is connected, -1 is not connected            
     
-class commandline(object):
-    def __init__(self):
-        self.my_input = raw_input()
 
-def commands():
 
-    test = commandline()
-                                                                
-    if test.my_input== "a":
-        print("entering auto printout mode\n")
-        cmdstate = "autoprint"
-     
-    if test.my_input== "e":    
-        cmdstate = "exiting"
 
-    else :
-        cmdstate = "commandin"
-
-    return cmdstate
 
 
 
