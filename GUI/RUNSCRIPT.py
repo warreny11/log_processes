6596f5833f6ca61a8fcbac4c5b266a59eb3acef4
Warren Yuan <warrenyuan@Warrens-MacBook-Pro.local>
2018-08-09 16:17:44 -0700

    redelegating info

--- GUI/RUNSCRIPT.py
+++ GUI/RUNSCRIPT.py
@@ -1,77 +1,19 @@
-from data_sort import convert
-import sys 
-import serial
+from SCRIPT import Connection,port,baud,Commands,Autoprint
 
-class Connection():
-    def __init__(self,port,baud):
-        self.port = port
-        self.baud = baud
-        print "Initializing Connection..."
-
-    def connect(self):
-        self.ser = serial.Serial(str(port), baud)
-        while self.ser.is_open:
-            return 0
-        else:
-            return -1
-
-print "help"
-port = "/dev/tty.usbserial"
-baud = 9600
-
-print "Hello"
 my_SL1_connection = Connection(port,baud)
 my_SL1_connection.connect()
-print my_SL1_connection
 
 if my_SL1_connection.connect()==0:
     print "Connected..."
 
+while my_SL1_connection.connect()==0:
+    print "bob"
 
 
-def Autoprint():
-    
-    rxstr = ''
-    out = ''
-    out += my_SL1_connection.ser.read()
-    rxstr += out
-    #if out != '':
-#            print (out)
-    if out == ';':
-        print(convert(rxstr))
-        rxstr = ''
 
 
-def commands(my_input):    
-    if my_input == "a":
-        commandstatus = "auto"
-        return commandstatus
-                
-    if my_input == "e":
-        commandstatus = "exit"
-        return commandstatus
-
-    else :
-        commandstatus = "free"
-        return my_input
-
-def commandstate(my_input):
-
-    if commands == "auto":
-        print "Entering Auto-update Mode..."
-        Autoprint()
-                
-    if commands == "exit":
-        print("Exiting program and disconnecting from Serial...")    #e: exit hotkey
-        my_SL1_connection.ser.close() 
-        sys.exit()
-
-    else :
-        my_SL1_connection.ser.write(my_input)
-
-while my_SL1_connection.ser.is_open:
-    commandstate(commands(raw_input()))
-    
+        
+        
 
 
 
--- GUI/SCRIPT.py
+++ GUI/SCRIPT.py
@@ -1,6 +1,10 @@
 import serial
+import sys
+from data_sort import convert
+import serial
 
-
+port = "/dev/tty.usbserial"
+baud = 9600
 
 class Connection():
     def __init__(self,port,baud):
@@ -14,3 +18,50 @@ class Connection():
             return 0
         else:
             return -1
+
+
+
+class Commands(Connection):
+    def __init__(self,my_input):
+        self.my_input = raw_input
+
+    def commands(self):    
+
+        if self.my_input == "a":
+            commandstatus = "auto"
+                    
+        if self.my_input == "e":
+            commandstatus = "exit"
+            
+        else :
+            commandstatus = "free"
+            
+        return commandstatus
+
+    def Autoprint(self):
+    
+        rxstr = ''
+        out = ''
+        out += self.ser.read()
+        rxstr += out
+        #if out != '':
+    #            print (out)
+        if out == ';':
+            print(convert(rxstr))
+            rxstr = ''
+
+    def executecommand(self,status): 
+    
+        if status == "auto":
+            print "Entering Auto-update Mode..."
+            Autoprint()
+                    
+        if status == "exit":
+            print("Exiting program and disconnecting from Serial...")    #e: exit hotkey
+            self.ser.close() 
+            sys.exit()
+
+        if status == "free":
+            self.ser.write(raw_input())
+
+   
