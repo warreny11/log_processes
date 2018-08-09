90d876da62103341a350d8c2501d8938748c39d9
Warren Yuan <warrenyuan@Warrens-MacBook-Pro.local>
2018-08-09 14:50:16 -0700

    fixes for while loop

--- GUI/RUNSCRIPT.py
+++ GUI/RUNSCRIPT.py
@@ -27,36 +27,50 @@ print my_SL1_connection
 if my_SL1_connection.connect()==0:
     print "Connected..."
 
+
+
 def Autoprint():
     
     rxstr = ''
-    while (1):
-        
-        out = ''
-        out += my_SL1_connection.ser.read()
-        rxstr += out
-        #if out != '':
+    out = ''
+    out += my_SL1_connection.ser.read()
+    rxstr += out
+    #if out != '':
 #            print (out)
-        if out == ';':
-            convert(rxstr)
-            rxstr = ''
+    if out == ';':
+        print(convert(rxstr))
+        rxstr = ''
 
-    
+
 def commands(my_input):    
-        if my_input == "a":
-            print "Entering Auto-update Mode..."
-            Autoprint()
-                    
-        if my_input == "e":
-            print("Exiting program and disconnecting from Serial...")    #e: exit hotkey
-            my_SL1_connection.ser.close() 
-            sys.exit()
+    if my_input == "a":
+        commandstatus = "auto"
+        return commandstatus
+                
+    if my_input == "e":
+        commandstatus = "exit"
+        return commandstatus
 
-        else :
-            my_SL1_connection.ser.write(my_input)
-    
+    else :
+        commandstatus = "free"
+        return my_input
+
+def commandstate(my_input):
+
+    if commands == "auto":
+        print "Entering Auto-update Mode..."
+        Autoprint()
+                
+    if commands == "exit":
+        print("Exiting program and disconnecting from Serial...")    #e: exit hotkey
+        my_SL1_connection.ser.close() 
+        sys.exit()
+
+    else :
+        my_SL1_connection.ser.write(my_input)
+
 while my_SL1_connection.ser.is_open:
-    commands(raw_input())
+    commandstate(commands(raw_input()))
     
 
 
