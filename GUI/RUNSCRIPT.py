a6c4f85568511bd10486d08d0b034a3a8e8358ff
Warren Yuan <warrenyuan@Warrens-MacBook-Pro.local>
2018-08-09 10:13:50 -0700

    trying to get ser to be intergrated

--- GUI/RUNSCRIPT.py
+++ GUI/RUNSCRIPT.py
@@ -1,17 +1,24 @@
 import SCRIPT
 
+debug = 1
 
+if debug == 0:
+    port = raw_input("Enter full port name: ")
+    baud = raw_input("Enter baud rate: ")
 
-baud = 9600
-port = "/dev/tty.usbserial"
+if debug == 1:
+    port = "/dev/tty.usbserial"
+    baud = 9600
+    ser = ""
 
-my_SL1_connection = SCRIPT.Connection(port,baud)
-my_SL1_connection.connect(port,baud)
+my_SL1_connection = SCRIPT.Connection(port,baud,"")
+my_SL1_connection.connect(port,baud,"")
+print my_SL1_connection.ser
 
-if my_SL1_connection.connect(port,baud)==0:
+if my_SL1_connection.connect(port,baud,ser)==0:
     print "Connected..."
     
-while my_SL1_connection.connect(port,baud)==0:
+while my_SL1_connection.connect(port,baud,ser)==0:
     my_input = raw_input()
     my_SL1_whileconnect = SCRIPT.Commands(my_input)
     my_SL1_whileconnect.commands(my_input)
--- GUI/SCRIPT.py
+++ GUI/SCRIPT.py
@@ -1,24 +1,48 @@
 import serial
-from autoprint import Autoprint,serwrite
 from exiting import leave
+from data_sort import convert
 
 debug = 0
 
 class Connection():
-    def __init__(self,port,baud):
+    def __init__(self,port,baud,ser):
         self.port = port
         self.baud = baud
+        self.ser = ser
+        
         print "Initializing Connection..."
 
-    def connect(self,a,b):
-        self.ser = serial.Serial(str(a), b)
+    def connect(self,port,baud,ser):
+        
+        self.ser = serial.Serial(str(port), baud)
         while self.ser.is_open:
             return 0
         else:
             return -1   
+    
+class InsideCommands(Connection):
+    def __init__(self,port,baud,ser):
+        Connection.__init__(self,port,baud,ser)
+    
 
 
-class Commands():
+    def Autoprint(self):
+        rxstr = ''
+        while (1):
+        
+            out = ''
+            out += self.ser.read()
+            rxstr += out
+            #if out != '':
+    #            print (out)
+            if out == ';':
+                convert(rxstr)
+                rxstr = ''
+
+    def serwrite(self,my_input):
+        serial.write(my_input)
+
+class Commands(InsideCommands):
     def __init__(self,my_input):
         self.my_input = my_input
         if debug==1:
@@ -28,14 +52,17 @@ class Commands():
     def commands(self,my_input):    
         if my_input == "a":
             print "Entering Auto-update Mode..."
-            Autoprint()
+            InsideCommands.Autoprint(self)
                     
         if my_input == "e":
             print "Exiting..."
             leave()
 
         else :
-            serwrite(my_input)
+            InsideCommands.serwrite(self,my_input)
+
+
+
         
 
 
