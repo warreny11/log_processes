fd3d93cc1f623db372e59f9920b3514dd1859436
Warren Yuan <warrenyuan@Warrens-MacBook-Pro.local>
2018-08-09 12:20:46 -0700

    reducing dependencies

--- GUI/RUNSCRIPT.py
+++ GUI/RUNSCRIPT.py
@@ -1,22 +1,18 @@
-import SCRIPT
+from SCRIPT import Connection
 from data_sort import convert
+import sys 
 
-debug = 1
+print "help"
+port = "/dev/tty.usbserial"
+baud = 9600
 
-if debug == 0:
-    port = raw_input("Enter full port name: ")
-    baud = raw_input("Enter baud rate: ")
+print "Hello"
+my_SL1_connection = Connection(port,baud)
+#my_SL1_connection.connect(port,baud)
+#print my_SL1_connection.ser
 
-if debug == 1:
-    port = "/dev/tty.usbserial"
-    baud = 9600
-
-my_SL1_connection = SCRIPT.Connection(port,baud)
-my_SL1_connection.connect(port,baud)
-print my_SL1_connection.ser
-
-if my_SL1_connection.connect(port,baud)==0:
-    print "Connected..."
+#if my_SL1_connection.connect(port,baud)==0:
+#    print "Connected..."
 
 def Autoprint():
     
@@ -32,14 +28,23 @@ def Autoprint():
             convert(rxstr)
             rxstr = ''
 
-def serwrite(my_input):
     
-    my_SL1_connection.ser.write(my_input)
+def commands(my_input):    
+        if my_input == "a":
+            print "Entering Auto-update Mode..."
+            Autoprint()
+                    
+        if my_input == "e":
+            print("Exiting program and disconnecting from Serial...")    #e: exit hotkey
+            my_SL1_connection.ser.close() 
+            sys.exit()
+
+        else :
+            my_SL1_connection.ser.write(my_input)
     
 while my_SL1_connection.connect(port,baud)==0:
-    my_input = raw_input()
-    my_SL1_whileconnect = SCRIPT.Commands(my_input)
-    my_SL1_whileconnect.commands(my_input)
+    commands(raw_input())
+    
 
 
 
--- GUI/SCRIPT.py
+++ GUI/SCRIPT.py
@@ -1,8 +1,6 @@
 import serial
-from RUNSCRIPT import Autoprint, serwrite
-from exiting import leave
 
-debug = 0
+
 
 class Connection():
     def __init__(self,port,baud):
@@ -13,26 +11,6 @@ class Connection():
     def connect(self,port,baud):
         self.ser = serial.Serial(str(port), baud)
         while self.ser.is_open:
-            return 0, self.ser
+            return 0
         else:
-            return -1   
-
-
-class Commands():
-    def __init__(self,my_input):
-        self.my_input = my_input
-        if debug==1:
-            print "Initializing Commands"
-        
-        
-    def commands(self,my_input):    
-        if my_input == "a":
-            print "Entering Auto-update Mode..."
-            Autoprint()
-                    
-        if my_input == "e":
-            print "Exiting..."
-            leave()
-
-        else :
-            serwrite(my_input)
+            return -1
