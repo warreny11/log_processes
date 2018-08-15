e4e755ad746fe48d13243169af6e876791af5538
warrenyuan <warreny7853@gmail.com>
2018-08-15 13:59:31 -0700

    changing self stuff

--- GUI/serialtesting.py
+++ GUI/serialtesting.py
@@ -6,67 +6,150 @@ import ttk
 import threading
 import time
 
-gui = Tk()
-gui.title("Seatrec Serial Interface")
+LARGE_FONT = ("Verdana", 12)
+NORM_FONT = ("Helvetica", 10)
+SMALL_FONT = ("Helvetica", 8)
 
-class Connection():
+def popupmsg(msg):
+    popup = Tk()
+    popup.wm_title("!")
+    label = ttk.Label(popup, text=msg, font=NORM_FONT)
+    label.pack(side="top", fill="x", pady=10)
+    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
 
-    def connect(self): 
-        version_ = button_var.get()
-        print version_
-        self.port = port_entry.get()
-        self.baud = baud_entry.get() 
+
+class SeatrecControlHub(Tk):
+
+    def __init__(self,*args,**kwargs):
+        
+        Tk.__init__(self, *args, **kwargs)
+
+        Tk.wm_title(self, "Seatrec Control Hub")
+
+        container = Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0,weight=1)
+        container.grid_columnconfigure(0,weight=1)
+
+        menubar = Menu(container)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Save settings", command = lambda: popupmsg("Not supported just yet!"))
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+
+        Tk.config(self, menu=menubar)
+
+        self.frames = {}
+
+        for F in (StartPage, Seatrec_Control_Hub):
+
+            frame = F(container,self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky = "nsew")
+
+        self.show_frame(StartPage)
 
         try:
-            if version_ == 2:
+            Tk.iconbitmap(self, default="seatrec_LQ1_icon.ico")
+        except:
+            # tk.Tk.iconbitmap(self, default="seatrec (1).png")
+            pass
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+    
+class StartPage(Frame):
+
+    def __init__(self,parent, controller):
+        Frame.__init__(self, parent)
+        label = Label(self,text = "Seatrec Status Checker : Connection to serial port required.", font = LARGE_FONT)
+        label.pack(side = "top", fill = "x", pady=10)
+
+        baud_label = Label(self,text = "Baud")
+        baud_label.pack(side = "top", fill = "x", pady=10)
+        port_label = Label(self,text = "Port (without tty)")
+        port_label.pack(side = "top", fill = "x", pady=10)
+
+        self.baud_entry = Entry(self,width = 7)
+        self.baud_entry.place(x = 100, y = 365)
+
+        self.port_entry = Entry(self,width = 7)
+        self.port_entry.place(x = 200, y = 365)
+
+        self.button_var = IntVar()
+
+        radio_1 = Radiobutton(text = "Windows", variable = self.button_var, value = 1)
+        radio_1.place(x = 10, y = 315)
+        radio_2 = Radiobutton(text = "Linux", variable = self.button_var, value = 2).place(x = 110, y = 315)
+        radio_2.place(x = 10, y = 315)
+        radio_3 = Radiobutton(text = "Mac", variable = self.button_var, value = 3).place(x = 210, y = 315)
+        radio_3.place(x = 10, y = 315)
+
+        self.version_ = self.button_var.get()
+       
+    def connect(self, parent,controller): 
+        Frame.__init__(self, parent)
+
+        connectbutton = Button(self,text = "Connect", command = self.connect)
+        connectbutton.place(x = 15, y = 360)
+
+        
+        print self.version_
+        self.port = self.port_entry.get()
+        self.baud = self.baud_entry.get() 
+
+        try:
+            if self.version_ == 2:
                 try:
                     ser = serial.Serial('/dev/tty' + str(self.port), self.baud)
                     if ser.is_open:
-                        print "Connected..."
+                        lambda: controller.show_frame(Seatrec_Control_Hub)
                 
                 except:
-                    print "Cant Open Specified Port"
+                    lambda: popupmsg("Unable to Connect")
 
-            elif version_ == 1:
+            elif self.version_ == 1:
                 try:
                     ser = serial.Serial('COM' + str(self.port), self.baud)
                     if ser.is_open:
-                        print "Connected..."
+                        lambda: controller.show_frame(Seatrec_Control_Hub)
 
                 except:
-                    print "Cant Open Specified Port"
+                    lambda: popupmsg("Unable to Connect")
 
-            elif version_ == 3:
+            elif self.version_ == 3:
                 try:
                     ser = serial.Serial('/dev/tty.' + str(self.port), self.baud)
                     if ser.is_open:
-                        print "Connected..."
+                        lambda: controller.show_frame(Seatrec_Control_Hub)
 
                 except:
-                    print "Cant Open Specified Port"
+                    lambda: popupmsg("Unable to Connect")
 
         except ValueError:
-            print "Enter Baud and Port"
+            lambda: popupmsg("Please Re-enter Port and Baud")
             return
+        
+class Seatrec_Control_Hub(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self,parent)
+        label = Label(self,text = "Seatrec Control Hub", font = LARGE_FONT)
+        label.pack(pady=10,padx=10)
 
 
-baud_entry = Entry(width = 7)
-baud_entry.place(x = 100, y = 365)
+        reconnectbutton = ttk.Button(self, text="Reconnect", command=lambda: controller.show_frame(StartPage))
+        reconnectbutton.pack()
 
-port_entry = Entry(width = 7)
-port_entry.place(x = 200, y = 365)
+ 
 
-button_var = IntVar()
-radio_1 = Radiobutton(text = "Windows", variable = button_var, value = 1).place(x = 10, y = 315)
-radio_2 = Radiobutton(text = "Linux", variable = button_var, value = 2).place(x = 110, y = 315)
-radio_3 = Radiobutton(text = "Mac", variable = button_var, value = 3).place(x = 210, y = 315)
-
-connection_ = Connection()
-
-connectbutton = Button(text = "Connect", command = connection_.connect).place(x = 15, y = 360)
-
-baud   = Label(text = "Baud").place(x = 100, y = 348)
-port   = Label(text = "Port (without tty)").place(x = 200, y = 348)
-
-gui.geometry('500x500')
-gui.mainloop()
+app = SeatrecControlHub()
+app.geometry("1280x720")
+app.mainloop()
