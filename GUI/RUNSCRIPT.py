4d1fcd04992d94a5450313c9fb0669a4def12242
warrenyuan <warreny7853@gmail.com>
2018-08-20 11:38:22 -0700

    added GUI resources

--- GUI/RUNSCRIPT.py
+++ GUI/RUNSCRIPT.py
@@ -2,8 +2,11 @@ import serial
 import time
 from SCRIPT import Connection
 
+port = raw_input("port: ")
+baud = raw_input("baud: ") 
+
 my_SL1_connection = Connection()
-Connect_status = my_SL1_connection.connect()
+Connect_status = my_SL1_connection.connect(port,baud)
 
 if Connect_status==0:
     print "Connected..."
